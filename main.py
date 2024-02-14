7ca3550031cf28cf7f0be273bc9d5413b3f12cb2
Steve Valles <savallesquiroz@mail.fhsu.edu>
2024-02-14 17:10:46 +0100

    Data from csv file stored on data variable. Changed StudentsPerformance.csv for world_suicide_rate_2023.csv instead

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # INF601 - Advanced Programming in Python
 # Steve Valles Quiroz
 # Mini Project 2
 
+#(5/5 points) Proper import of packages used.
 import pandas as pd
 
-#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
-#(5/5 points) Proper import of packages used.
 #(20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
    #Think of some question you would like to solve such as:
    #"How many homes in the US have access to 100Mbps Internet or more?"
    #"How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
    #Here are some other great datasets: https://www.kaggle.com/datasets
+
+data = pd.read_csv('world_suicide_rate_2023.csv', index_col='Country')
+
+print(data.head())
 #(10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 #(10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 #(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
